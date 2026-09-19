c51494c2faf92e9424f52c75581f7d3a05b1ed82
oxdc <29519076+oxdc@users.noreply.github.com>
2026-09-19 18:06:12 +0800

    fix: prevent context menu actions from triggering on Vertical Tabs

--- src/managers/FileIconManager.ts
+++ src/managers/FileIconManager.ts
@@ -16,12 +16,15 @@ export default class FileIconManager extends IconManager {
 
 	constructor(plugin: IconicPlugin) {
 		super(plugin);
-		this.plugin.registerEvent(this.app.workspace.on('file-menu', (_, tFile) => {
+		this.plugin.registerEvent(this.app.workspace.on('file-menu', (menu, tFile) => {
+			// Skip if the 'file-menu' event is triggered by Vertical Tabs via `onPaneMenu(..., "more-options")`
+			if ((menu as { isVTMenu?: boolean })?.isVTMenu) return;
 			if (this.plugin.settings.showMenuActions) {
 				this.onContextMenu(tFile.path);
 			}
 		}));
-		this.plugin.registerEvent(this.app.workspace.on('files-menu', (_, tFiles) => {
+		this.plugin.registerEvent(this.app.workspace.on('files-menu', (menu, tFiles) => {
+			if ((menu as { isVTMenu?: boolean })?.isVTMenu) return;
 			if (this.plugin.settings.showMenuActions) {
 				this.onContextMenu(...tFiles.map(tFile => tFile.path));
 			}
